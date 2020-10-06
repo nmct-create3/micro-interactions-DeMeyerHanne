35f0491d8e08ac8f59706880ced2e75f74b2d4d9
DeMeyerHanne <55881988+DeMeyerHanne@users.noreply.github.com>
2020-10-06 12:22:47 +0200

    labo3deel1

--- script/app.js
+++ script/app.js
@@ -2,9 +2,41 @@ function handleFloatingLabel() {
    
 }
 
-function handlePasswordSwitcher() {
+const handlePasswordSwitcher = function() {
+    const passwordInput = document.querySelector('.js-password-input');
+    const passwordToglle = document.querySelector('.js-password-toggle');
     
+    const passwordOptions = ['text', 'password'];
+
+    // simpele zelfcontrole
+    if (!passwordInput || !passwordToglle) {
+        console.error('De classes werden niet gevonden', passwordInput , passwordToglle);
+        return;
+    }
+
+    // we gaan luisteren of er geklikt wordt op de checkbox (oogje)
+    passwordToglle.addEventListener('change', function() {
+        // als er geklikt wordt, veranderen we het type van de input van 'password' naar 'text' en vice versa
+        passwordInput.type = passwordOptions[+this.checked];
+
+            // zelfde idee, maar niet goed genoeg
+        // if(passwordInput.type == 'password') {
+        //     passwordInput.type = 'text'
+        // } else {
+        //     passwordInput.type = 'password';
+        // }
+    })
 }
+
+const getDOMElements = function() {
+    const errorMessage = document.querySelector('.');
+    const input = document.querySelector('.');
+    const field = document.querySelector('.')
+}
+
+let email = {js-email},
+password = {},
+signInButton;
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
